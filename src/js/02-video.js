d5153ba066c926a28f5a2a643bf77baa303dc9a9
YuliaOd <106181095+YuliaOd@users.noreply.github.com>
2022-11-05 07:50:21 +0300

    change 05.11

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,15 @@ import { throttle } from 'lodash';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate',  throttle( e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
-    }, 1000)
-    );
-
-player
-.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-.catch(function (error) {
-    console.error(error)
-});
+player.on('timeupdate', throttle(onPlay, 1000));
+   
+function onPlay ({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds)
+}
+setCurrentTime()
+function setCurrentTime(){
+    if(!localStorage.getItem('videoplayer-current-time')){
+        return
+    }
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+}
